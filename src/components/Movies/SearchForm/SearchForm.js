09b8adeb25ae97af4b86a64f1a99a11a4cd2e1bd
anastasia <nastja1994@yandex.ru>
2021-10-29 09:52:28 +0300

    feat: сделать компонент SearchForm

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -7,16 +7,20 @@ function SearchForm() {
   return(
     <section className='section search'>
       <form className='search__form'>
-        <fieldset className='search__fieldset'>
-          <div className='search__icon'/>
-          <input className='search__field' id='search' type='text' placeholder='Фильм'/>
-        </fieldset>
-        <button className='search__button' />
-        <label for='short-film' className='search__switch'>
-          <input type='checkbox' id='short-film'/>
-          <span className="search__slider"/>
-        </label>
-        <span>Короткометражки</span>
+        <div className='search__main-container'>
+          <fieldset className='search__fieldset'>
+            <div className='search__icon'/>
+            <input className='search__field' id='search' type='text' placeholder='Фильм'/>
+          </fieldset>
+          <button className='search__button' />
+        </div>
+        <div className='search__switch-container'>
+          <label for='short-film' className='search__switch'>
+            <input type='checkbox' id='short-film'/>
+            <span className='search__slider'/>
+          </label>
+          <span className='search__shortfilm'>Короткометражки</span>
+        </div>
         </form>
     </section>
   )
