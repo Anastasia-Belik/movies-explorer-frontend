2f6933dacce9eb51bb307b262be800a43161680c
anastasia <nastja1994@yandex.ru>
2021-11-05 19:01:24 +0300

    feat: добавить логику отображения кнопки на разных страницах

--- src/components/Movies/MoviesCard/MoviesCard.js
+++ src/components/Movies/MoviesCard/MoviesCard.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { useLocation } from "react-router";
 
 import './MoviesCard.css';
 
+
 function MoviesCard(props) {
+
+  const location = useLocation();
+  let isSaved = false;
+
+  let saveButtonClassName;
+  if(location.pathname === '/saved-movies') {
+    saveButtonClassName = 'delete';
+  } else {
+    saveButtonClassName = isSaved ? 'saved' : 'notsaved';
+  }
+
+
   return(
     <li className='movies-card'>
       <div className='movies-card__header'>
@@ -10,7 +24,10 @@ function MoviesCard(props) {
           <h2 className='movies-card__name' title={props.data.name}>{props.data.name}</h2>
           <p className='movies-card__duration'>{props.data.duration}</p>
         </div>
-        <button className='movies-card__icon movies-card__icon_type_notsaved' type='button'/>
+        <button
+          className={`movies-card__icon movies-card__icon_type_${saveButtonClassName}`}
+          type='button'
+        />
       </div>
       <img className='movies-card__image' src={props.data.image} alt={props.data.name}/>
     </li>
