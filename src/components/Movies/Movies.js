cf9b21cea97e43d85aa9fa94dbdbbe0a94a4bbf4
anastasia <nastja1994@yandex.ru>
2021-11-06 10:16:17 +0300

    feat: добавить мобильную версию Profile

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-import './Movies.css'
 import Footer from "../Footer/Footer";
 import SearchForm from "./SearchForm/SearchForm";
 import MoviesCardList from "./MoviesCardList/MoviesCardList";
