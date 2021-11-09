933911c4dee908a9c4831fc297e27e3beb390571
anastasia <nastja1994@yandex.ru>
2021-11-09 12:20:11 +0300

    feat: добавить текст при отсутствии результата или ошибке при поиске

--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -14,6 +14,8 @@ function MoviesCardList(props) {
           />
         ))}
       </ul>
+      <p className="cards__no-result">По вашему запросу ничего не найдено, попробуйте еще раз</p>
+      <p className="cards__server-error">Упс, на сайте что-то поломалось, подождите немного и попробуйте еще раз</p>
       <div className='cards__button-container'>
         <button className='cards__button'>Еще</button>
       </div>
