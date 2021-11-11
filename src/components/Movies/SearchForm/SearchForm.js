df38bd86e756a6045495b437eda75764d339a681
anastasia <nastja1994@yandex.ru>
2021-11-11 18:27:52 +0300

    feat: добавить валидацию формы поиска

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -2,17 +2,43 @@ import React from "react";
 
 import './SearchForm.css'
 
+import { useFormWithValidation } from '../../../utils/validation';
 
 function SearchForm() {
+
+  const {
+    values,
+    handleChange,
+    errors,
+    isValid,
+    resetForm
+  } = useFormWithValidation();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (values['searchInput']) {
+      console.log(values['searchInput']);
+      resetForm();
+    }
+  }
+
   return(
     <section className='section search'>
-      <form className='search__form'>
+      <form className='search__form' name='searchForm' noValidate onSubmit={handleSubmit}>
         <div className='search__main-container'>
           <fieldset className='search__fieldset'>
             <div className='search__icon'/>
-            <input className='search__field' id='search' type='text' placeholder='Фильм'/>
+            <input className='search__field'
+                   id='search'
+                   type='text'
+                   placeholder='Фильм'
+                   name='searchInput'
+                   value={values.searchInput || ''}
+                   onChange={handleChange}
+                   required
+            />
           </fieldset>
-          <button className='search__button' />
+          <button className={`search__button ${!isValid && 'search__button_inactive'}`} type='submit'/>
         </div>
         <div className='search__switch-container'>
           <label className='search__switch'>
@@ -22,6 +48,7 @@ function SearchForm() {
           <span className='search__shortfilm'>Короткометражки</span>
         </div>
         </form>
+      <p className='search__input-error'>{errors.searchInput}</p>
     </section>
   )
 }
