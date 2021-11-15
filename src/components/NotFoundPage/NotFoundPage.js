14714feae086d951f5358f33f2c5dad2ccf4d7c3
anastasia <nastja1994@yandex.ru>
2021-11-15 14:56:57 +0300

    fix: поправить ошибку со страницей 404

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -6,17 +6,13 @@ import {useHistory} from "react-router";
 function NotFoundPage() {
   const history = useHistory();
 
-  function goBack() {
-    history.goBack()
-  }
-
   return (
     <section className='nfpage'>
       <div className='nfpage__content'>
         <h1 className='nfpage__code'>404</h1>
         <h2 className='nfpage__message'>Страница не найдена</h2>
       </div>
-      <button className='nfpage__back' onClick={goBack}>Назад</button>
+      <button className='nfpage__back' onClick={history.goBack}>Назад</button>
     </section>
   )
 }
