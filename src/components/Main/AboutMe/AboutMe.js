a45eee0d7b92c4f7fbb15dae741d1e6f8d114c67
anastasia <nastja1994@yandex.ru>
2021-12-03 19:38:16 +0300

    fix: обновить персданные

--- src/components/Main/AboutMe/AboutMe.js
+++ src/components/Main/AboutMe/AboutMe.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import './AboutMe.css';
-import photo from '../../../images/pic__COLOR_pic.png';
+import photo from '../../../images/photo.jpeg';
 import Portfolio from "./Portfolio/Portfolio";
 
 function AboutMe() {
@@ -11,18 +11,17 @@ function AboutMe() {
       <div className='about-me__container'>
         <img src={photo} alt='Студент' className='about-me__photo'/>
         <div className='about-me__info'>
-          <h3 className='about-me__name'>Виталий</h3>
-          <h4 className='about-me__description'>Фронтенд-разработчик, 30 лет</h4>
+          <h3 className='about-me__name'>Анастасия</h3>
+          <h4 className='about-me__description'>Фронтенд-разработчик, 27 лет</h4>
           <p className='section-text about-me__text'>
-            Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
-            и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить.
-            С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке,
-            начал заниматься фриланс-заказами и ушёл с постоянной работы.
+            Я родилась и живу в Москве. Начинала карьеру в банковской сфере,
+            затем попала в IT-среду и заинтересовалась разработкой. Нравится делать красивые,
+            удобные и функциональные интерфейсы, поэтому выбрала Frontend.
           </p>
           <ul className='about-me__socials'>
-            <li><a href='https://www.facebook.com/' className='about-me__link' rel="noreferrer"
+            <li><a href='https://www.facebook.com/profile.php?id=100004359156977' className='about-me__link' rel="noreferrer"
                    target="_blank">Facebook</a></li>
-            <li><a href='https://github.com/' className='about-me__link' rel="noreferrer" target="_blank">GitHub</a>
+            <li><a href='https://github.com/Anastasia-Belik' className='about-me__link' rel="noreferrer" target="_blank">GitHub</a>
             </li>
           </ul>
         </div>
